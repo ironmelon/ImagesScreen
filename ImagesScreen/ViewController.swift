843c15a22bbcd136d3fdd933d77d7eb5d6fbbcdd
Dynnikov Oleg <dinnikovoleg@gmail.com>
2018-07-18 09:54:38 +0300

    Added functionality for buttons and imageviews in VC.

--- ImagesScreen/ViewController.swift
+++ ImagesScreen/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView1: UIImageView!
+    @IBOutlet weak var imageView2: UIImageView!
+    @IBOutlet weak var imageView3: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func showAllButton(_ sender: Any) {
+        imageView1.isHidden = false
+        imageView2.isHidden = false
+        imageView3.isHidden = false
+    }
+    @IBAction func hideFirstButton(_ sender: Any) {
+        imageView1.isHidden = true
+    }
+    @IBAction func hideSecondButton(_ sender: Any) {
+        imageView2.isHidden = true
+    }
+    @IBAction func hideThirdButton(_ sender: Any) {
+        imageView3.isHidden = true
+    }
+    
 }
 
